966148f5864994ef726f3148396160ac07bd5a6c
Manu Varkey <manuvarkey@gmail.com>
2022-11-22 13:24:51 +0530

    Add drag and drop support

--- gelectrical/__init__.py
+++ gelectrical/__init__.py
@@ -235,6 +235,38 @@ class MainWindow():
         self.set_title(window_title)
         # Hide fileselector
         self.builder.get_object('popup_open').hide()
+        
+    def drag_data_received(self, widget, context, x, y, selection, target_type, timestamp):
+        if target_type == 80:
+            data_str = selection.get_data().decode('utf-8')
+            uri = data_str.strip('\r\n\x00')
+            file_uri = uri.split()[0] # we may have more than one file dropped
+            filename = misc.get_file_path_from_dnd_dropped_uri(file_uri)
+            
+            if os.path.isfile(filename):
+                # Ask confirmation from user
+                if self.stack.haschanged():
+                    message = 'You have unsaved changes which will be lost if you continue.\n Are you sure you want to discard these changes ?'
+                    title = 'Confirm Open'
+                    dialogWindow = Gtk.MessageDialog(self.window,
+                                             Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                                             Gtk.MessageType.QUESTION,
+                                             Gtk.ButtonsType.YES_NO,
+                                             message)
+                    dialogWindow.set_transient_for(self.window)
+                    dialogWindow.set_title(title)
+                    dialogWindow.set_default_response(Gtk.ResponseType.NO)
+                    dialogWindow.show_all()
+                    response = dialogWindow.run()
+                    dialogWindow.destroy()
+                    if response != Gtk.ResponseType.YES:
+                        # Do not open file
+                        log.info('MainWindow - drag_data_received - Cancelled by user')
+                        return
+                        
+                # Open file
+                self.open_project(filename)
+                log.info('MainApp - drag_data_received  - opnened file ' + filename)
 
     def on_save(self, button):
         """Save project to file already opened"""
@@ -884,6 +916,12 @@ class MainWindow():
         
         # Setup about dialog
         self.about_dialog = self.builder.get_object("aboutdialog")
+        
+        # Darg-Drop support for files
+        self.window.drag_dest_set( Gtk.DestDefaults.MOTION | Gtk.DestDefaults.HIGHLIGHT | Gtk.DestDefaults.DROP,
+                  [Gtk.TargetEntry.new("text/uri-list", 0, 80)], 
+                  Gdk.DragAction.COPY)
+        self.window.connect('drag-data-received', self.drag_data_received)
         
         self.window.show_all()
         log.info('MainWindow - Initialised')
